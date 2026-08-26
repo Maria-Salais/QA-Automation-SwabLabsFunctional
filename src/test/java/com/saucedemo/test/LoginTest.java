12a70d7516669baca7f31514840653173190adb2
MSalais <tu>
2026-08-25 23:42:41 -0600

    Test

--- src/test/java/com/saucedemo/test/LoginTest.java
+++ src/test/java/com/saucedemo/test/LoginTest.java
@@ -32,7 +32,7 @@ public class LoginTest extends TestBase {
         LoginPage loginPage = new LoginPage(driver, OR);
         loginPage.login(inputData.getProperty("validUsernameValue"), inputData.getProperty("validPasswordValue"));
 
-        test.log(Status.PASS, "Login Exitoso");
+        test.log(Status.PASS, "Login Exitoso1");
         test.pass(MediaEntityBuilder.createScreenCaptureFromPath(utils.captureScreen(this.driver, config.getProperty("testEvidencePath"))).build());
 
         Thread.sleep(2000);
